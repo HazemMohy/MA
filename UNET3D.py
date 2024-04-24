f4c9356bc4012ba97e918f1a3797157771e58a2a
HazemMohy <44666165+HazemMohy@users.noreply.github.com>
2024-04-24 15:10:42 +0200

    Update UNET3D.py

--- UNET3D.py
+++ UNET3D.py
@@ -42,7 +42,7 @@ class Net(pytorch_lightning.LightningModule):
         self._model = UNet(
             spatial_dims=3,
             in_channels=2,
-            out_channels=2,
+            out_channels=1,
             channels=(16, 32, 64, 128, 256),
             strides=(2, 2, 2, 2),
             num_res_units=2,
