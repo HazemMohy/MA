eaa6b256fdd9b3102e5031d8da1752dbde622a39
HazemMohy <44666165+HazemMohy@users.noreply.github.com>
2024-06-24 11:15:32 +0300

    import warnings

--- 3D_Classification.py
+++ 3D_Classification.py
@@ -73,10 +73,14 @@ import monai
 import json
 import os
 ##################################
+import warnings
+warnings.filterwarnings("ignore")  # remove some scikit-image warnings
+##################################
 #The pin_memory parameter in DataLoader is used to speed up data transfer between the host (CPU) and the GPU. When pin_memory is set to True, it allows the data loader to use pinned (page-locked) memory, which can make data transfer
 #to the GPU faster. This is particularly useful when training models on a GPU.
 pin_memory = torch.cuda.is_available()
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:0")
+#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 # print_config()
@@ -190,3 +194,80 @@ train_loader = DataLoader(train_ds, batch_size=1, shuffle=True, num_workers=2, p
 val_ds = ImageDataset(image_files=shuffled_paths[-3:], labels=shuffled_labels[-3:], transform=val_transforms)
 val_loader = DataLoader(val_ds, batch_size=1, num_workers=2, pin_memory=pin_memory)
 ##################################
+# standard PyTorch program style
+# Create DenseNet121, CrossEntropyLoss and Adam optimizer. THEN, start the Training & Evaluation 
+
+print("Create Model")
+model = monai.networks.nets.DenseNet121(spatial_dims=3, in_channels=1, out_channels=2).to(device)
+
+print("Create Loss")
+loss_function = torch.nn.CrossEntropyLoss()
+#loss_function = torch.nn.BCEWithLogitsLoss()  # also works with this data
+#loss_function = DiceCELoss(include_background=True, to_onehot_y=True, sigmoid=True) #MONAI-DiceCELoss #try softmax #CHANGE #it is NOT PURELY binary cross entropy loss
+
+print("Create Optimizer")
+learning_rate = 1e-4
+optimizer = torch.optim.Adam(model.parameters(), learning_rate)
+
+print("Training and Evaluation started!")
+val_interval = 1
+best_metric = -1
+best_metric_epoch = -1
+epoch_loss_values = []
+metric_values = []
+writer = SummaryWriter()
+max_epochs = 5 #just for the testing phase
+
+
+for epoch in range(max_epochs):
+    print("-" * 10)
+    print(f"epoch {epoch + 1}/{max_epochs}")
+    model.train()
+    epoch_loss = 0
+    step = 0
+
+    for batch_data in train_loader:
+        step += 1
+        inputs, labels = batch_data[0].to(device), batch_data[1].to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = loss_function(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.item()
+        epoch_len = len(train_ds) // train_loader.batch_size
+        print(f"{step}/{epoch_len}, train_loss: {loss.item():.4f}")
+        writer.add_scalar("train_loss", loss.item(), epoch_len * epoch + step)
+
+    epoch_loss /= step
+    epoch_loss_values.append(epoch_loss)
+    print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
+
+    if (epoch + 1) % val_interval == 0:
+        model.eval()
+
+        num_correct = 0.0
+        metric_count = 0
+        for val_data in val_loader:
+            val_images, val_labels = val_data[0].to(device), val_data[1].to(device)
+            with torch.no_grad():
+                val_outputs = model(val_images)
+                value = torch.eq(val_outputs.argmax(dim=1), val_labels.argmax(dim=1))
+                metric_count += len(value)
+                num_correct += value.sum().item()
+
+        metric = num_correct / metric_count
+        metric_values.append(metric)
+
+        if metric > best_metric:
+            best_metric = metric
+            best_metric_epoch = epoch + 1
+            torch.save(model.state_dict(), "best_metric_model_classification3d_array.pth")
+            print("saved new best metric model")
+
+        print(f"Current epoch: {epoch+1} current accuracy: {metric:.4f} ")
+        print(f"Best accuracy: {best_metric:.4f} at epoch {best_metric_epoch}")
+        writer.add_scalar("val_accuracy", metric, epoch + 1)
+
+print(f"Training completed, best_metric: {best_metric:.4f} at epoch: {best_metric_epoch}")
+writer.close()
