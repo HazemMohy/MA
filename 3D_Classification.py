54760393d4ae65c7143a841fab5bb1774cb9e9be
HazemMohy <44666165+HazemMohy@users.noreply.github.com>
2024-06-24 11:55:08 +0300

    Dataset instead of ImageDataset

--- 3D_Classification.py
+++ 3D_Classification.py
@@ -215,77 +215,77 @@ print(type(im), im.shape, label, label.shape)
 # standard PyTorch program style
 # Create DenseNet121, CrossEntropyLoss and Adam optimizer. THEN, start the Training & Evaluation 
 
-print("Create Model")
-model = monai.networks.nets.DenseNet121(spatial_dims=3, in_channels=1, out_channels=2).to(device)
+# print("Create Model")
+# model = monai.networks.nets.DenseNet121(spatial_dims=3, in_channels=1, out_channels=2).to(device)
 
-print("Create Loss")
-loss_function = torch.nn.CrossEntropyLoss()
-#loss_function = torch.nn.BCEWithLogitsLoss()  # also works with this data
-#loss_function = DiceCELoss(include_background=True, to_onehot_y=True, sigmoid=True) #MONAI-DiceCELoss #try softmax #CHANGE #it is NOT PURELY binary cross entropy loss
+# print("Create Loss")
+# loss_function = torch.nn.CrossEntropyLoss()
+# #loss_function = torch.nn.BCEWithLogitsLoss()  # also works with this data
+# #loss_function = DiceCELoss(include_background=True, to_onehot_y=True, sigmoid=True) #MONAI-DiceCELoss #try softmax #CHANGE #it is NOT PURELY binary cross entropy loss
 
-print("Create Optimizer")
-learning_rate = 1e-4
-optimizer = torch.optim.Adam(model.parameters(), learning_rate)
+# print("Create Optimizer")
+# learning_rate = 1e-4
+# optimizer = torch.optim.Adam(model.parameters(), learning_rate)
 
-print("Training and Evaluation started!")
-val_interval = 1
-best_metric = -1
-best_metric_epoch = -1
-epoch_loss_values = []
-metric_values = []
-writer = SummaryWriter()
-max_epochs = 5 #just for the testing phase
+# print("Training and Evaluation started!")
+# val_interval = 1
+# best_metric = -1
+# best_metric_epoch = -1
+# epoch_loss_values = []
+# metric_values = []
+# writer = SummaryWriter()
+# max_epochs = 5 #just for the testing phase
 
 
-for epoch in range(max_epochs):
-    print("-" * 10)
-    print(f"epoch {epoch + 1}/{max_epochs}")
-    model.train()
-    epoch_loss = 0
-    step = 0
+# for epoch in range(max_epochs):
+#     print("-" * 10)
+#     print(f"epoch {epoch + 1}/{max_epochs}")
+#     model.train()
+#     epoch_loss = 0
+#     step = 0
 
-    for batch_data in train_loader:
-        step += 1
-        inputs, labels = batch_data[0].to(device), batch_data[1].to(device)
-        optimizer.zero_grad()
-        outputs = model(inputs)
-        loss = loss_function(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        epoch_loss += loss.item()
-        epoch_len = len(train_ds) // train_loader.batch_size
-        print(f"{step}/{epoch_len}, train_loss: {loss.item():.4f}")
-        writer.add_scalar("train_loss", loss.item(), epoch_len * epoch + step)
+#     for batch_data in train_loader:
+#         step += 1
+#         inputs, labels = batch_data[0].to(device), batch_data[1].to(device)
+#         optimizer.zero_grad()
+#         outputs = model(inputs)
+#         loss = loss_function(outputs, labels)
+#         loss.backward()
+#         optimizer.step()
+#         epoch_loss += loss.item()
+#         epoch_len = len(train_ds) // train_loader.batch_size
+#         print(f"{step}/{epoch_len}, train_loss: {loss.item():.4f}")
+#         writer.add_scalar("train_loss", loss.item(), epoch_len * epoch + step)
 
-    epoch_loss /= step
-    epoch_loss_values.append(epoch_loss)
-    print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
+#     epoch_loss /= step
+#     epoch_loss_values.append(epoch_loss)
+#     print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
 
-    if (epoch + 1) % val_interval == 0:
-        model.eval()
+#     if (epoch + 1) % val_interval == 0:
+#         model.eval()
 
-        num_correct = 0.0
-        metric_count = 0
-        for val_data in val_loader:
-            val_images, val_labels = val_data[0].to(device), val_data[1].to(device)
-            with torch.no_grad():
-                val_outputs = model(val_images)
-                value = torch.eq(val_outputs.argmax(dim=1), val_labels.argmax(dim=1))
-                metric_count += len(value)
-                num_correct += value.sum().item()
+#         num_correct = 0.0
+#         metric_count = 0
+#         for val_data in val_loader:
+#             val_images, val_labels = val_data[0].to(device), val_data[1].to(device)
+#             with torch.no_grad():
+#                 val_outputs = model(val_images)
+#                 value = torch.eq(val_outputs.argmax(dim=1), val_labels.argmax(dim=1))
+#                 metric_count += len(value)
+#                 num_correct += value.sum().item()
 
-        metric = num_correct / metric_count
-        metric_values.append(metric)
+#         metric = num_correct / metric_count
+#         metric_values.append(metric)
 
-        if metric > best_metric:
-            best_metric = metric
-            best_metric_epoch = epoch + 1
-            torch.save(model.state_dict(), "best_metric_model_classification3d_array.pth")
-            print("saved new best metric model")
+#         if metric > best_metric:
+#             best_metric = metric
+#             best_metric_epoch = epoch + 1
+#             torch.save(model.state_dict(), "best_metric_model_classification3d_array.pth")
+#             print("saved new best metric model")
 
-        print(f"Current epoch: {epoch+1} current accuracy: {metric:.4f} ")
-        print(f"Best accuracy: {best_metric:.4f} at epoch {best_metric_epoch}")
-        writer.add_scalar("val_accuracy", metric, epoch + 1)
+#         print(f"Current epoch: {epoch+1} current accuracy: {metric:.4f} ")
+#         print(f"Best accuracy: {best_metric:.4f} at epoch {best_metric_epoch}")
+#         writer.add_scalar("val_accuracy", metric, epoch + 1)
 
-print(f"Training completed, best_metric: {best_metric:.4f} at epoch: {best_metric_epoch}")
-writer.close()
+# print(f"Training completed, best_metric: {best_metric:.4f} at epoch: {best_metric_epoch}")
+# writer.close()
