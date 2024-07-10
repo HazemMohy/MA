9feb3d8ea123c3c4ebda00832d5511f51f7e91ea
HazemMohy <44666165+HazemMohy@users.noreply.github.com>
2024-07-10 19:05:07 +0200

    max_epochs = 500

--- 3D_Classification.py
+++ 3D_Classification.py
@@ -299,17 +299,17 @@ print("Define dataset loaders")
 # train_ds = ImageDataset(image_files=shuffled_paths[:7], labels=shuffled_labels[:7], transform=train_transforms) #Creates a dataset object using ImageDataset with the shuffled paths and labels, applying the train_transforms.
 # train_loader = DataLoader(train_ds, batch_size=4, shuffle=True, num_workers=2, pin_memory=pin_memory) #Creates a data loader for the dataset. pin_memory=pin_memory enables pinned memory if a GPU is available.
 train_ds = Dataset(data=train_files, transform=train_transforms)
-train_loader = DataLoader(train_ds, batch_size=1, shuffle=True, num_workers=4)#, pin_memory=pin_memory) #test --> batch_size = 2
+train_loader = DataLoader(train_ds, batch_size=2, shuffle=True, num_workers=4)#, pin_memory=pin_memory) #test --> batch_size = 2
 
 # Create a validation data loader
 # val_ds = ImageDataset(image_files=shuffled_paths[-3:], labels=shuffled_labels[-3:], transform=val_transforms)
 # val_loader = DataLoader(val_ds, batch_size=4, num_workers=2, pin_memory=pin_memory)
 val_ds = Dataset(data=val_files, transform=val_transforms)
-val_loader = DataLoader(val_ds, batch_size=1, num_workers=2)#, pin_memory=pin_memory)
+val_loader = DataLoader(val_ds, batch_size=2, num_workers=2)#, pin_memory=pin_memory)
 
 # Create a testing data loader
 test_ds = Dataset(data=test_files, transform=test_transforms)
-test_loader = DataLoader(test_ds, batch_size=1, num_workers=2)
+test_loader = DataLoader(test_ds, batch_size=2, num_workers=2)
 
 
 # # Check first data loader output
@@ -399,7 +399,7 @@ best_metric_epoch = -1
 epoch_loss_values = []
 metric_values = []
 writer = SummaryWriter()
-max_epochs = 5 #test --> 500 or 1000
+max_epochs = 500 #test --> 500 or 1000
 
 
 for epoch in range(max_epochs):
@@ -499,8 +499,8 @@ print(f"Test accuracy: {test_accuracy:.4f}")
 # Save output and error at Runs
 slurm_output_file = f"/lustre/groups/iterm/Hazem/MA/HPC/slurm_outputs/3D_Seg_{slurm_job_id}_output.txt"
 slurm_error_file = f"/lustre/groups/iterm/Hazem/MA/HPC/slurm_outputs/3D_Seg_{slurm_job_id}_error.txt"
-run_slurm_output_file = os.path.join(run_dir, f"3D_Classification_{slurm_job_id}_0_output_{best_metric:.4f}_{test_accuracy:.4f}.txt")
-run_slurm_error_file = os.path.join(run_dir, f"3D_Classification_{slurm_job_id}_0_error_{best_metric:.4f}_{test_accuracy:.4f}.txt")
+run_slurm_output_file = os.path.join(run_dir, f"3D_Classification_{slurm_job_id}_0_output_{best_metric:.4f}_{best_metric_epoch}_{test_accuracy:.4f}.txt")
+run_slurm_error_file = os.path.join(run_dir, f"3D_Classification_{slurm_job_id}_0_error_{best_metric:.4f}_{best_metric_epoch}_{test_accuracy:.4f}.txt")
 
 shutil.copy(slurm_output_file, run_slurm_output_file)
 shutil.copy(slurm_error_file, run_slurm_error_file)
