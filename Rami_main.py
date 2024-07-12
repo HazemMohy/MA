b3d7c2f62795026bcab8c4fbb58bab0b1fb2b6e4
HazemMohy <44666165+HazemMohy@users.noreply.github.com>
2024-07-12 06:47:55 +0200

    test

--- Rami_main.py
+++ Rami_main.py
@@ -136,7 +136,7 @@ def create_instance_scores(path_gt, path_prediction, path_result, network_name):
     return df_scores
 
 # Name of your network
-network_name    = "Hazem_Test_2"
+network_name    = "Hazem_Test"
 
 # Paths 
 path_gt         = "/lustre/groups/iterm/Hazem/MA/Runs/run_21121056__DiceCELoss_none/Rami_Voxel_gt" # Path to ground truth
