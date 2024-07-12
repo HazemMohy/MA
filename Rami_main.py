cfc9951fd0fa5c0c5a8ee052ef379891712a8f69
HazemMohy <44666165+HazemMohy@users.noreply.github.com>
2024-07-12 05:59:51 +0200

    created paths

--- Rami_main.py
+++ Rami_main.py
@@ -134,12 +134,12 @@ def create_instance_scores(path_gt, path_prediction, path_result, network_name):
     return df_scores
 
 # Name of your network
-network_name    = ""
+network_name    = "Hazem_Test"
 
 # Paths 
-path_gt         = "" # Path to ground truth
-path_prediction = "" # Path to prediction
-path_result     = "" # Path where result csv is saved to
+path_gt         = "/lustre/groups/iterm/Hazem/MA/Runs/run_21121056__DiceCELoss_none/Rami_Voxel_gt" # Path to ground truth
+path_prediction = "/lustre/groups/iterm/Hazem/MA/Runs/run_21121056__DiceCELoss_none/Model_outputs_nifti" # Path to prediction
+path_result     = "/lustre/groups/iterm/Hazem/MA/Runs/run_21121056__DiceCELoss_none/Model_outputs_nifti" # Path where result csv is saved to
 
 create_voxel_scores(path_gt, path_prediction, path_result, network_name)
 
